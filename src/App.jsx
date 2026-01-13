f9edb9cf3e6b41d3515fa639fa37d789f4a6a0d8
tolanny-studio <hellotolannystudio@gmail.com>
2026-01-13 23:00:20 +0100

    Add tracking page to react

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage'
 import { Routes,Route } from 'react-router'
 import CheckoutPage from './pages/CheckoutPage'
 import OrdersPage from './pages/OrdersPage'
+import TrackingPage from './pages/TrackingPage'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route index element={<HomePage/>} />
       <Route path='/checkout' element={<CheckoutPage/>} />
       <Route path='/orders' element={<OrdersPage />}/>
+      <Route path='/tracking' element={< TrackingPage/>}/>
     </Routes>
     
   )
