9b8c19f0643bc35183563c0209bb5acd3ac31340
tolanny-studio <hellotolannystudio@gmail.com>
2026-01-14 14:57:51 +0100

    Add 404 Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes,Route } from 'react-router'
 import CheckoutPage from './pages/checkout/CheckoutPage'
 import OrdersPage from './pages/OrdersPage'
 import TrackingPage from './pages/TrackingPage'
+import Page404 from './pages/Page404'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path='/checkout' element={<CheckoutPage/>} />
       <Route path='/orders' element={<OrdersPage />}/>
       <Route path='/tracking' element={< TrackingPage/>}/>
+      <Route path='*' element={<Page404/>}/>
     </Routes>
     
   )
---
+++ src/pages/Page404.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Page404 = () => {
+  const defaultStyle = {
+    display:"flex",
+    fontSize: '5rem',
+    justifyContent: 'center',
+    alignItems:'center',
+    height:'100vh',
+    color:'rgb(8, 79, 45)'
+  }
+  return (
+    <div style={defaultStyle}>
+      <h1 >404Page</h1>
+    </div>
+    
+  )
+}
+
+export default Page404
