28fc1ed1df0f399d26093f0182d6d3f9f1aba41b
tolanny-studio <hellotolannystudio@gmail.com>
2026-01-14 09:51:25 +0100

    Create checkout folder in pages for all checkouts, and separate checkout header into react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
  import './App.css'
 import HomePage from './pages/HomePage'
 import { Routes,Route } from 'react-router'
-import CheckoutPage from './pages/CheckoutPage'
+import CheckoutPage from './pages/checkout/CheckoutPage'
 import OrdersPage from './pages/OrdersPage'
 import TrackingPage from './pages/TrackingPage'
 
